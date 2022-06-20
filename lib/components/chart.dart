9d3f780d5e33cfc18a4e62e13070c2ab5d2b7517
kevin caires <kevin.caires@smartgreen.net>
2022-06-20 15:48:23 -0300

    ADD chart bar.

--- lib/components/chart.dart
+++ lib/components/chart.dart
@@ -1,7 +1,58 @@
 import 'package:flutter/material.dart';
-import 'package:personal_spent/models/transaction.dart';
 import 'package:intl/intl.dart';
 import '../models/transaction.dart';
+
+class ChartBar extends StatelessWidget {
+  final String label;
+  final double value;
+  final double percentage;
+
+  ChartBar({
+    required this.label,
+    required this.value,
+    required this.percentage,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Text(value.toStringAsFixed(2)),
+        const SizedBox(height: 5),
+        Container(
+          height: 60,
+          width: 10,
+          child: Stack(
+            alignment: Alignment.bottomCenter,
+            children: <Widget>[
+              Container(
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.grey,
+                    width: 1.0,
+                  ),
+                  color: const Color.fromRGBO(220, 220, 220, 1),
+                  borderRadius: BorderRadius.circular(5),
+                ),
+              ),
+              FractionallySizedBox(
+                heightFactor: percentage,
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Theme.of(context).colorScheme.primary,
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+        const SizedBox(height: 5),
+        Text(label),
+      ],
+    );
+  }
+}
 
 class Chart extends StatelessWidget {
   final List<Transaction> recentTransactions;
@@ -39,18 +90,13 @@ class Chart extends StatelessWidget {
       margin: EdgeInsets.all(20),
       child: Row(
         children: groupedTransactions.map((tr) {
-          return Text('${tr['day']}: ${tr['value']}');
+          return ChartBar(
+            label: tr['day'].toString(),
+            value: double.parse(tr['value'].toString()),
+            percentage: 0.5,
+          );
         }).toList(),
       ),
     );
-  }
-}
-
-class ChartBar extends StatelessWidget {
-  // weekday
-  // total
-  @override
-  Widget build(BuildContext context) {
-    return Card();
   }
 }
